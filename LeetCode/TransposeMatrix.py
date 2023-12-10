dffcac4229eab83fc5e4320afd7013c07565d078
Nadim Bouassida <ndaemm@gmail.com>
2023-12-10 18:21:18 +0100

    added TranspodeMatrix.py

---
+++ LeetCode/TransposeMatrix.py
@@ -0,0 +1,22 @@
+"""
+867. Transpose Matrix
+Given a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+
+Example 2:
+Input: matrix = [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]
+ 
+"""
+def transpose(matrix):
+        result = [matrix[j][i] for i in range(len(matrix)) for j in range(len(matrix[i]))]
+        return result
+
+test_cases = [[[1,2,3],[4,5,6],[7,8,9]],[[1,2,3],[4,5,6]]]
+for test in test_cases:
+        print(transpose(test))
