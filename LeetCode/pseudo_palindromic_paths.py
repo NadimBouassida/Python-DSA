cedd268de94bd5964c60e22a50e08112881ace8e
Nadim Bouassida <ndaemm@gmail.com>
2024-01-25 16:10:27 +0100

    Added pseudo_palindromic_paths.py problem N° 1457 but not solved yet

---
+++ LeetCode/pseudo_palindromic_paths.py
@@ -0,0 +1,37 @@
+from binary_tree_helper import TreeNode, to_binary_tree
+from typing import Optional
+
+
+class Solution:
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+
+        path = set([root.val])
+        
+        return self.isPalindrom(root, path)
+
+        
+
+
+    def isPalindrom(self, node, path):
+        if not node:
+            return 0
+
+        if not node.left and not node.right:
+            if len(path) <= 1:
+                return 1
+            else:
+                return 0
+
+        if node.val in path:
+            path.remove(node.val)
+        else:
+            path.add(node.val)
+
+        
+        return self.isPalindrom(node.left,path) + self.isPalindrom(node.right,path)
+
+root = to_binary_tree([8,8,None,7,7,None,None,2,4,None,8,None,7,None,1])
+
+s = Solution()
+
+print(s.pseudoPalindromicPaths(root))
