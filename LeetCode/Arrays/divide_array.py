c7445f57ef70f409975e26cd8a03894aa14bae9b
Rania Khalifa <Rania@ATLAS.LOCAL>
2024-02-01 13:38:37 +0100

    added divide_array.py still looking for solution

--- LeetCode/Arrays/divide_array.py
+++ LeetCode/Arrays/divide_array.py
@@ -1,4 +1,8 @@
 """
 2966. Divide Array Into Arrays With Max Difference
 """
+from typing import List
 
+class Solution:
+    def divide_array(self, nums: List[int], k: int) -> List[List[int]]:
+        pass
