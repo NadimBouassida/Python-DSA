256c3a659cf7e8342a12a7fed74993e3b8e14403
Nadim Bouassida <ndaemm@gmail.com>
2024-02-01 19:24:09 +0100

    divide_array.py found solution

--- LeetCode/Arrays/divide_array.py
+++ LeetCode/Arrays/divide_array.py
@@ -5,4 +5,23 @@ from typing import List
 
 class Solution:
     def divide_array(self, nums: List[int], k: int) -> List[List[int]]:
-        pass
+        if len(nums) < 3:
+            return []
+        nums.sort()
+        answer = []
+        prev = 0
+        for i in range(3,len(nums),3):
+            answer.append(nums[prev:i])
+            prev = i
+        answer.append(nums[-3:])
+
+        for ans in answer:
+            for i in range(len(ans)-1):
+                for j in range(i,len(ans)):
+                    if ans[j] - ans [i] > k:
+                        return [] 
+
+        return answer 
+
+s = Solution()
+print(s.divide_array([1,3,4,8,7,9,3,5,1],2))
