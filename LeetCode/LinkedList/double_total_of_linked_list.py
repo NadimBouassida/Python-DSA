d54b37b503db92c43731a1ccaac5b8898b2c08c6
Rania Khalifa <Rania@ATLAS.LOCAL>
2024-05-07 14:54:52 +0100

    2816. Double a Number Represented as a Linked List

--- LeetCode/LinkedList/double_total_of_linked_list.py
+++ LeetCode/LinkedList/double_total_of_linked_list.py
@@ -4,19 +4,21 @@ from linked_list_helper import list_to_sllist, ListNode, print_llist
 
 
 class Solution(object):
-    
     def doubleIt(self, head):
-        curr = head 
-        if curr.val > 4: 
-            head = ListNode(1, head) 
-            # ans = ListNode(1, head)
-        while curr.next: 
-            curr.val = (curr.val * 2 + (curr.next.val > 4)) % 10
-            curr = curr.next 
-        curr.val = (curr.val * 2) % 10
+        prv = ListNode()
+        prv.next = head
+        dummy = prv
+        while head:
+            head.val *= 2
+            if head.val >= 10:
+                head.val -= 10
+                prv.val += 1
 
-        return head
+            prv = head
+            head = head.next
 
+        return dummy if dummy.val > 0 else dummy.next
+    
 lst = [9,9,9]
 head = list_to_sllist(lst)
 
